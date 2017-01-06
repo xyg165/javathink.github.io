92ba7345695555c96cb9b6831da322b2a9583c2c
xyg165 <xyg165@126.com>
2017-01-06 17:24:36 +0800

    mythinkingjava
    Signed-off-by: xyg165 <xyg165@126.com>

---
+++ src/think/c02/HelloWorld.java
@@ -0,0 +1,22 @@
+//c02:HelloWorld.java from Thinking in Java,2nd ed.
+package think.c02;
+
+import java.util.Date;
+
+/**
+ * @Description:The first Thinking in Java example program.Displays a string and today's date.
+ * @author xyg 2017-1-6
+ * @email  xyg165@126.com
+ * @version 1.0
+ */
+public class HelloWorld {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.print("HelloWorld is created in:");
+		System.out.println(new Date());
+	}
+
+}
